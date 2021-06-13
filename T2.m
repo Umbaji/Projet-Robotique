891deda57ccc65f791195aae40bc830be1f25be5
ESSOWE JUSTIN BAKOUBOLO <71730008+Justobak01@users.noreply.github.com>
2021-06-13 05:39:35 +0200

    Update T2.m
    Optimisation du code

--- T2.m
+++ T2.m
@@ -1,5 +1,7 @@
-clear ; close all ; clc ;
-%initialisation
+clear ; close all ; clc ; %initialisation
+
+%Définition des variables articulaires (Longueurs et angles)
+
 OA = 352 ; AB = 360 ; BC = 360 ; CP = 85 ;
 
 l1 = AB ; l2 = BC ; l3 = CP ;
@@ -10,6 +12,8 @@ theta3 = linspace(115,-115,100);
 
 [THETA1 , THETA2 , THETA3] = meshgrid (theta1,theta2,theta3);%matrice comportant les angle
 
+%Calcul des coordonnées des centres des axes.
+
 Bx = l1*cosd(THETA1);
 By = OA + l1*sind(THETA1);
     
@@ -19,9 +23,6 @@ Cy = By + l2*sind(THETA1 + THETA2);
 X = Cx + l3*cosd(THETA1 + THETA2 + THETA3);
 Y = Cy + l3*sind(THETA1 + THETA2 + THETA3);
 
-data1 = [X(:) Y(:) THETA1(:)];
-data2 = [X(:) Y(:) THETA2(:)];
-data3 = [X(:) Y(:) THETA3(:)];
 
 plot(X(:),Y(:),'r.')
 axis equal;
